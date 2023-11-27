cbaf44abd6ade0e1e84c65470eebe2da747da8ce
DoKkangs <88784852+DoKkangs@users.noreply.github.com>
2023-11-27 16:58:56 +0900

    [level unrated] Title: 최댓값 만들기 (1), Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/unrated/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        for(int i = 0 ; i < numbers.length - 1 ; i++){
+        for(int i = 0 ; i < numbers.length ; i++){
             int temp = 0;
             for(int j = i + 1 ; j < numbers.length ; j++){
                 temp = numbers[i] * numbers[j];
