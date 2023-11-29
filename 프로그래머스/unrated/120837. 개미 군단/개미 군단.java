a705d5e2f4716cca62d8961d9fade12c594ab6b3
DoKkangs <88784852+DoKkangs@users.noreply.github.com>
2023-11-29 15:26:36 +0900

    [level unrated] Title: 개미 군단, Time: 0.02 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.java
@@ -1,16 +1,7 @@
 class Solution {
     public int solution(int hp) {
         int answer = 0;
-        while(hp != 0){
-            answer += hp / 5;
-            hp %= 5;
-            
-            answer += hp / 3;
-            hp %= 3;
-            
-            answer += hp / 1;
-            hp %= 1;
-        }
+        answer = hp / 5 + (hp % 5) / 3 + (hp % 5) % 3  ;
         return answer;
     }
 }
