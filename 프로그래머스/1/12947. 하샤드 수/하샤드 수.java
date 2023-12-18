e94537bc71bc42d5c59898f196b9dc0ed98ecade
KimDoHyeon <88784852+DoKkangs@users.noreply.github.com>
2023-12-18 20:39:47 +0900

    [level 1] Title: 하샤드 수, Time: 0.12 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
-        int temp = x;
         int h = 0;
-        while (temp > 0){
-            h+=temp%10;
-            temp/=10;
+        String[] list = String.valueOf(x).split("");
+        for (String str : list) {
+            h += Integer.parseInt(str);
         }
-        System.out.println(h);
         answer = (x % h == 0) ? true : false ;
         return answer;
     }
