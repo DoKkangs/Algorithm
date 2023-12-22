f90b964ac2086a1b20ce44ffcbf901eb0c4b2347
KimDoHyeon <88784852+DoKkangs@users.noreply.github.com>
2023-12-22 10:13:00 +0900

    [level unrated] Title: 각도기, Time: 0.02 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120829. 각도기/각도기.java
+++ 프로그래머스/unrated/120829. 각도기/각도기.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        if(angle < 90){
-            answer = 1;
-        } else if (angle == 90) {
-            answer = 2;
-        } else if (90 < angle && angle < 180) {
-            answer = 3;
-        } else if( angle == 180 ){
-            answer = 4;
-        } else {
-            answer = 0;
-        }
+        int answer = (angle < 90) ? 1 : (angle == 90) ? 2 : ( angle < 180 ) ? 3 : (angle == 180) ? 4 : -1;
         return answer;
     }
 }
