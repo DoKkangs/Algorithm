7d5a6228ef373bcecac294579e067300bcced24a
KimDoHyeon <88784852+DoKkangs@users.noreply.github.com>
2023-12-29 09:10:24 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 9.50 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -2,8 +2,9 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         char[] c = s.toCharArray();
-        if(c.length % 2 == 0) answer = "" + c[c.length/2-1] + c[c.length/2];
-        else answer = "" + c[c.length/2];
+        if(c.length % 2 == 0){
+            answer = "" + c[c.length/2-1] + c[c.length/2];
+        }else answer = "" + c[c.length/2];
         return answer;
     }
 }
