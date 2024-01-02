72cce0aee3f030fa6bf60762dfce96ad5a414dd5
KimDoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-01-02 10:22:52 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 70.07 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -2,8 +2,6 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         answer[0]=-1;
-        
-
         for(int i = 1 ; i < s.length() ; i++){
             answer[i] = -1;
             for(int j = 0 ; j < i ; j++){
