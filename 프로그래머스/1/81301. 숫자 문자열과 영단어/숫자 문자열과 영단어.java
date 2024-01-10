482a2bf7a8f187b59179428650dc4f4b89040c4e
DoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-01-10 15:54:40 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
+        // 0부터 9까지 숫자 매칭 배열
         String[] nums = {"0","1","2","3","4","5","6","7","8","9"};
+        // 0부터 9까지 단어 매칭 배열
         String[] words = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+        // for문으로 입력받은 문자열을 돌면서 단어가 발견되면 숫자 매칭 배열과 인덱스로 매칭해서 변환
         for (int i = 0; i < 10; i++) {
             s = s.replace(words[i],nums[i]);
         }
+        // 문자열을 숫자로 변환
         answer = Integer.parseInt(s);
         return answer;
     }
