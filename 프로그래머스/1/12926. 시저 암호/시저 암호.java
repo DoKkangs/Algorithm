40d22cc255117928cdf7001c36a38f0d8e97d115
DoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-01-10 15:50:04 +0900

    [level 1] Title: 시저 암호, Time: 31.85 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,7 +1,10 @@
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
+        // 받아온 배열을 단어 단위로 관리하기 위해 character 배열로 변환
         char[] str = s.toCharArray();
+        // 스페이스가 들어있으면 스페이스를 추가해주고 알파벳이 들어오면 n만큼 이동후
+        // 알파벳의 끝인 z를 넘어가면 모듈러 연산으로 a부터 시작하도록 해서 대소문자 변환
         for(char c : str){
             if(c == ' '){
                 answer += ' ';
