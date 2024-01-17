ba06c2cf9bd8e23c7a5c559fabac7cf73343cef4
DoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-01-17 09:34:33 +0900

    [level 1] Title: K번째수, Time: 0.56 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        // 커맨드의 개수가 answer 배열의 크기
         int[] answer = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {
+            //반복문 둘면서 커맨드의 범위 만큼 임시 배열 뽑기(배열의 인덱스는 0부터 시작 주의)
             int[] temp = Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+            // 만들어진 배열 정렬
             Arrays.sort(temp);
+            // 정렬된 배열 요소 몇번째인지 추가(배열은 인덱스가 0부터 시작하므로 커맨드에서 1을 빼준다) 
             answer[i] = temp[commands[i][2]-1];
         }
         return answer;
