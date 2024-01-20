75aa6f582b8af61b5b8928b254d13105138fddbb
DoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-01-21 00:18:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 6.40 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String num = String.valueOf(n);
+        String num = "" + n; 
         for (char number : num.toCharArray()) {
             answer += Character.getNumericValue(number);;
         }
