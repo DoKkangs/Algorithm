9522960af2dd29f9928209a081f29ad81cbc4c53
DoHyeon <88784852+DoKkangs@users.noreply.github.com>
2024-03-11 12:11:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -5,10 +5,9 @@ class Solution{
         
         // 한 게임에 들어있을 때 까지 반복
         while(a != b) {
-            
 			a = (a / 2) + (a % 2); //짝수면 나누기 2, 홀수면 나누기 2 + 1
 			b = (b / 2) + (b % 2);
-		
+            
 			answer++; //게임 수 증가
 		}
         return answer;
